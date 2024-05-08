cafc6e8b01878f03ea012fa9a6a4c9ddd161d0e6
Krishna Singh <id.krishnasingh@gmail.com>
2024-05-08 06:34:03 +0530

    print all subarrays

---
+++ 2.Functions and arrays /_14SubArrayProblemGQ.java
@@ -0,0 +1,33 @@
+// this solution O(n^2) is better than the solution on pepcoding O(n^3)
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class _14SubArrayProblemGQ {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        printSubArray(arr);
+    }
+
+    public static void printSubArray(int[] arr){
+        System.out.println("<-------------------------Output------------------------->");
+        for(int i = 0; i < arr.length; i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j = i; j< arr.length; j++){
+                sb.append(arr[j]);
+                sb.append(" ");
+                System.out.println(sb);
+            }
+        }
+    }
+}
