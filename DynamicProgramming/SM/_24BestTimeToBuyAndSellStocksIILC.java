698476a7e299f6f90cf00c9878e48dd4e015c8b7
Krishna Singh <id.krishnasingh@gmail.com>
2024-10-29 09:00:32 +0530

    dp question using dp 😅

--- DynamicProgramming/SM/_24BestTimeToBuyAndSellStocksIILC.java
+++ DynamicProgramming/SM/_24BestTimeToBuyAndSellStocksIILC.java
@@ -1,5 +1,8 @@
 package SM;
 
+// It can be done in multiple ways: 1 sliding window 2. Dynamic programming
+// As of now coming to the solution intuition wise, DP is preferred.
+
 
 // Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 
@@ -23,6 +26,74 @@ public class _24BestTimeToBuyAndSellStocksIILC {
         return result;
     }
 }
+
+
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         // int canBuy = 1; // 1= yes
+
+//         // Integer[][] mem = new Integer[2][prices.length];
+//         // return solution(prices, 0, 1, mem);
+
+//         // // Tabulation
+//         // int[][] tab = new int[2][prices.length + 1];
+
+//         // // row = 0 (canBuy = false) ; row = 1 (canBuy = true)
+//         // tab[0][prices.length] = 0;
+//         // tab[1][prices.length] = 0;
+
+//         // for(int i = prices.length - 1; i >= 0; i--){
+//         //     tab[0][i] = Math.max(tab[1][i + 1] + prices[i], tab[0][i + 1]);
+//         //     tab[1][i] = Math.max(tab[0][i + 1] - prices[i], tab[1][i + 1]);
+//         // }
+
+//         // return tab[1][0];
+
+
+//         //Space optimisation
+//         //observation, In the array that we have created for Tabulisation. How many previous states 
+//         // we need to calculate current state? only 1(previous one), we are not going beyond that for 
+//         // calculating current state. whenever no. of states are limited (one, two, etc) space
+//         //  optimisation can be done.
+
+//         int canBuy = 0, canSell = 0;
+
+//         for(int i = prices.length - 1; i >= 0; i--){
+//             int newCanBuy = Math.max(canBuy, canSell - prices[i]);
+//             int newCanSell = Math.max(canSell, canBuy + prices[i]);
+
+//             canBuy = newCanBuy;
+//             canSell = newCanSell;
+//         }
+
+//         return canBuy;
+        
+//     }
+
+//     public int solution(int[] prices, int i, int canBuy, Integer[][] mem ){
+//         if(i == prices.length)  return 0;
+
+//         if(Objects.nonNull(mem[canBuy][i])){
+//             return mem[canBuy][i];
+//         }
+
+//         int max = solution(prices, i + 1, canBuy, mem);
+
+//         if(canBuy == 1){
+//             int buy = solution(prices, i + 1, 0, mem);
+//             max= Math.max(max, buy - prices[i]);
+//         }else{
+//             int sell = solution(prices, i + 1, 1, mem);
+//             max = Math.max(max, sell + prices[i]);
+//         }
+
+//         mem[canBuy][i] = max;
+//         return max;
+//     }
+// }
+
+
 
 
 
--- DynamicProgramming/SM/_25BestTimeToBuyAndSellTransaction.java
+++ DynamicProgramming/SM/_25BestTimeToBuyAndSellTransaction.java
@@ -1,5 +1,4 @@
 package SM;
-import java.util.Scanner;
 
 // Sample Input:
 // 12
@@ -10,30 +9,22 @@ import java.util.Scanner;
 // maximum profit: 13
 public class _25BestTimeToBuyAndSellTransaction {
     public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-
-        int[] arr = new int[n];
-
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = scn.nextInt();
-        }
-
-        int t = scn.nextInt();
+        int[] arr = {10, 15, 17, 20, 16, 18, 22, 20, 22, 20, 23, 25};
+        int t = 3;
 
         int maxProfit = bestTimeToBuyAndSellTransaction(arr, t);
         System.out.println("maximum profit: " + maxProfit);
     }
 
-    //tf: transaction fees
-    //obsp: old buy stock profit
-    //ossp: old sell stock profit
-    public static int bestTimeToBuyAndSellTransaction(int[] prices, int tf){
-        int obsp = - prices[0], ossp = 0;
+    // tf: transaction fees
+    // obsp: old buy stock profit
+    // ossp: old sell stock profit
+    public static int bestTimeToBuyAndSellTransaction(int[] prices, int tf) {
+        int obsp = -prices[0], ossp = 0;
 
-        for(int i = 1; i < prices.length; i++){
+        for (int i = 1; i < prices.length; i++) {
             int nbsp = Math.max(obsp, ossp - prices[i]);
-            int nssp = Math.max(ossp, prices[i] + obsp - tf );
+            int nssp = Math.max(ossp, prices[i] + obsp - tf);
 
             obsp = nbsp;
             ossp = nssp;
